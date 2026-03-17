a18ea90d895653b53b94b2baf8fcbf5e0987a3fd
yashdarji236 <yashdarji5237@gmail.com>
2026-03-17 18:27:51 +0530

    Learning Socket IO

---
+++ Day-31/server.js
@@ -0,0 +1,29 @@
+import app from './src/app.js'
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+  // ...
+  console.log("new connection created");
+
+  socket.on('message',(msg)=>{
+    console.log("User message is fired");
+    console.log(msg);
+    io.emit("abc")
+    
+  })
+  
+});
+
+//socket.emit()
+//socket.broadcast().emit()
+//io.emit()
+
+httpServer.listen(3000,()=>{
+    console.log("Server is running");
+    
+})
