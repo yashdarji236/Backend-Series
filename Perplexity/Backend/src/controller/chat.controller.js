cdc267bb8875a9f1b3bfc6e6bb5a3784e5e9bfb7
yashdarji236 <yashdarji5237@gmail.com>
2026-04-23 20:12:48 +0530

    refactor: clean up SendMessage function by removing console log and adjusting formatting

--- Perplexity/Backend/src/controller/chat.controller.js
+++ Perplexity/Backend/src/controller/chat.controller.js
@@ -6,15 +6,16 @@ import messageModel from "../models/message.model.js";
 export async function SendMessage(req, res) {
     const { message } = req.body;
     const title = await GeneratetheTitle(message)
-    console.log("Generated Title:", title);
     const result = await GenerateResponce(message)
+
+    
 
     const chat = await chatModel.create({
         user: req.user.id,
         title
     })
     const userMessage = await messageModel.create({
-        chat:chat._id,
+        chat: chat._id,
         content: message,
         role: 'user'
     })
@@ -29,6 +30,7 @@ export async function SendMessage(req, res) {
         title,
         userMessage,
         AiMessage
-    })
+    
+     })
 
 }
