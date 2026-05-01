9789eb270ac6c7d3af21cbed88f387e0ba2e9cd7
yashdarji236 <yashdarji5237@gmail.com>
2026-05-01 21:59:07 +0530

    fix: update API base URL to production for authentication and chat services

--- Perplexity/Frontend/src/featues/chat/service/chat.api.js
+++ Perplexity/Frontend/src/featues/chat/service/chat.api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { store } from '../../../app.store';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://perplexity-72qa.onrender.com',
   withCredentials: true,
 });
 
